13f6f5650c9a2fe142784a8406999f25a1b34fc1
Lorenzo Monacelli <mesonepigreco@gmail.com>
2023-07-29 13:10:18 +0200

    Some functions

--- src/Minnie.jl
+++ src/Minnie.jl
@@ -1,5 +1,13 @@
 module Minnie
 
-greet() = print("Hello World!")
+const G2_LEN  = 2
+const G4_LEN  = 4
+
+struct SymmetriFunction{T}
+    n_g2 :: Int
+    n_g4 :: Int
+    vector_g2 :: Matrix{T}
+    vector_g4 :: Matrix{T}
+end
 
 end # module Minnie
