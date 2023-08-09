0473024b7a67cc53e0bfd37a0b97fcfaf93b8480
Lorenzo Monacelli <mesonepigreco@gmail.com>
2023-08-09 20:32:28 +0200

    Some fixes in embedded.jl

--- src/embedding.jl
+++ src/embedding.jl
@@ -1,66 +1,69 @@
 using LinearAlgebra
 using CUDA
 using Flux
+using Zygote: @ignore
+using Flux.Optimise: update!, Descent
+using Flux: params
+
+using ChainRulesCore
+using ChainRulesCore: rrule
+
 """
-Module to encode the embedding of the atomic species.
-"""
+Module to encode the embedding of the atomic species. """
 
 
-function get_input_layer(coords, atomic_species, n_species)
-    # Prepare the embedding for the atomic species
-    
-    embedding = Embedding(n_species => 4)
+# Create the neural network
+n_species = 2
+n_embedding = 2
+n_atoms = 2
+n_dims = 1
+n_cutoff = 2
 
+struct CustomModel
+    embedding
+    DNN
 end
 
+function get_dist(coords :: Matrix{T}) where T
+    n_rand = size(coords, 1)
+    distance = zeros(T, 1, n_rand)
+    distance[1, :] = abs.(coords[:, 1] .- coords[:, 2])
+    return distance
+end
 
-@doc raw"""
-    build_network(n_species, n_embedding, n_cutoff)
-
-Builds the network that will be used to predict the energy of the system.
-"""
-function build_network(n_species, n_embedding, n_cutoff)
-    embedding = Embedding(n_species => n_embedding)
-    DNN = Chain(
-                Dense(n_embedding + 2*n_cutoff, 32, relu),
-                Dense(32, 16, relu),
-                Dense(16, 1)
-                )
-
-    function network(coords, atomic_species)
-        # Prepare the embedding for the atomic species
-
-        # Prepare the descriptors
-        n_atoms = size(coords, 1)
-        descriptors = zeros(n_cutoff, n_atoms, 2)
-        atomic_environment = zeros(n_atoms)
-
-        distances = zeros(n_atoms)
-        atomic_layers = []
-
-        output = 0.0
-        for i in 1:n_atoms
-            distances .= 0
-            for j in i+1:n_atoms
-                distances[j] = norm(coords[i, :] - coords[j, :])
+function ChainRulesCore.rrule(::typeof(get_dist), coords :: Matrix{T}) where T
+    function pullback(∇dist)
+        n_rand = size(coords, 1)
+        ∇coords = zeros(T, size(coords))
+        for i in 1:n_rand
+            if coords[i, 1] > coords[i, 2]
+                ∇coords[i, 1] = ∇dist[1, i]
+                ∇coords[i, 2] = -∇dist[1, i]
+            else
+                ∇coords[i, 1] = -∇dist[1, i]
+                ∇coords[i, 2] = ∇dist[1, i]
             end
-            atomic_environment = embedding(atomic_species)
-            atomic_environment = sortperm(distances)
-            sorted_distances = sort(distances)
+        end 
 
-            sort_dist = 1.0 ./ sorted_distances[1:n_cutoff]
-            atoms =  atomic_environment[1:n_cutoff]
-
-            # Concatenate the embedding with the coordinates
-            input_layer = Flux.cat(embedding(atomic_species[i]), sort_dist, atoms, dims=1)
-            energy += DNN(input_layer)
-        end
-
-        return output
-    end
-
-    return network
+        return NoTangent(), ∇coords
+    end 
+    return get_dist(coords), pullback
 end
+
+function (m::CustomModel)(coords, atomic_species)
+    n_rand = size(coords)[1]
+    distance = get_dist(coords)
+    
+    # Concatenate the embedding with the coordinates
+    emblayer = m.embedding(permutedims(atomic_species, (2,1)))
+    println("SIZE: ", size(emblayer))
+    input_layer = Flux.cat(permutedims(emblayer, (3, 1, 2)), distance, dims=1)
+    output = m.DNN(input_layer)
+
+    return output
+end
+
+Flux.@functor CustomModel
 
 
 function lennard_jones(r, sigma, epsilon)
@@ -69,7 +72,7 @@ end
 
 function potential(coords, species, sigma_dict, epsilon_dict)
     # Compute the potential energy of the system
-    n_atoms = size(coords, 1)
+    coords = reshape(coords, n_atoms, n_dims)
     energy = 0.0
     for i in 1:n_atoms
         for j in i+1:n_atoms
@@ -82,22 +85,52 @@ function potential(coords, species, sigma_dict, epsilon_dict)
     return energy
 end
 
+
 function generate_train_set(n_random, n_species, n_atoms, dimensions)
-    coords = randn(dimensions, n_random)
-    species = rand(1:n_species, n_random)
+    coords = randn(Float32, n_random, n_atoms * dimensions)
+    species = rand(1:n_species, n_random, n_atoms)
 
-    sigma_dict = Dict(1 => 1.0, 2 => 2.0)
-    epsilon_dict = Dict(1 => 1.0, 2 => 2.0)
+    sigma_dict = Dict(1 => Float32(1.0), 2 => Float32(2.0))
 
-    energies = zeros(n_random)
-    forces = zeros(dimensions*n_atoms, n_random)
+    epsilon_dict = Dict(1 => Float32(1.0), 2 => Float32(2.0))
+
+    energies = zeros(Float32, n_random)
 
     for i in 1:n_random
-        energies[i] = potential(coords[:, i], species, sigma_dict, epsilon_dict)
+        energies[i] = potential(coords[i, :], species[i, :], sigma_dict, epsilon_dict)
     end
 
+    return coords, species, energies
+end
 
 
+embedding = Embedding(n_species => n_embedding)
+DNN = Chain(
+            Dense(n_embedding + 1, 32, relu),
+            Dense(32, 16, relu),
+            Dense(16, 1)
+            )
+
+model = CustomModel(embedding, DNN)
+
+coords, species, energies = generate_train_set(10000, n_species, n_atoms, n_dims)
+
+η = 0.01
+function loss(coords, species, energies)
+    pred_energy = model(coords, species)
+    return sum((pred_energy .- energies).^2)
+end
 
 
-
+# for iter in 1:100
+#     grads = gradient(params(model)) do
+#         loss(coords, species, energies)
+#     end
+# 
+#     for p in params(model)
+#         update!(p, -η * grads[p])
+#     end
+# 
+#     println("$iter    $(loss(coords, species, energies))")
+# end
+#ju 
